1209783a6c24adda45d6e4ffe3b97d216951b93d
YMaher99 <youssefmaher725@gmail.com>
2022-12-06 01:39:54 +0200

    Added Cart Activity finalized Restaurant Activity

--- app/src/main/java/com/example/wagba/MainActivity.java
+++ app/src/main/java/com/example/wagba/MainActivity.java
@@ -13,9 +13,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, RestaurantsActivity.class);
+        Intent intent = new Intent(this, RestaurantActivity.class);
         startActivity(intent);
-
 
     }
 }
